15d3d2811d645391d51274d95426a86b91133bdb
efreetsultan <krizsan.robert.szv@gmail.com>
2022-10-11 12:49:00 +0200

    stuff added

--- client/src/Employees.jsx
+++ client/src/Employees.jsx
@@ -2,13 +2,22 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Loading from "./Loading";
-import {Button, Table, TableRow, TableBody, TableCell, TableContainer, TableHead, Paper, Tooltip} from '@mui/material/';
-import {Delete, Upgrade} from '@mui/icons-material';
+import {
+  Button,
+  Table,
+  TableRow,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  Paper,
+  Tooltip,
+} from "@mui/material/";
+import { Delete, Upgrade } from "@mui/icons-material";
 
 const fetchEmployees = () => {
   return fetch("/api/employees").then((res) => res.json());
 };
-
 
 const deleteEmployee = (id) => {
   return fetch(`/api/employees/${id}`, {
@@ -20,6 +29,8 @@ export default function Employees() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
+  const [filteredEmployees, setFilteredEmployees] = useState("")
+  const[filter, setFilter]=useState("")
 
   const handleDelete = (id) => {
     deleteEmployee(id).catch((error) => {
@@ -30,12 +41,19 @@ export default function Employees() {
     });
   };
 
+  const handleFilter = (event) => {
+    setFilteredEmployees(event.target.value)
+    const filteredData = [...data]
+    setFilter(filteredData.filter((x) => x.height > event.target.value))
+  }
+
   useEffect(() => {
     fetchEmployees()
       .then((employees) => {
         setData(employees);
         setLoading(false);
         setError(null);
+        setFilter(employees);
       })
       .catch((error) => {
         setData(null);
@@ -45,6 +63,15 @@ export default function Employees() {
       });
   }, []);
 
+  const handleRandomize = () => {
+    return fetch("/api/employees/height", {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((employees) => {
+        setData(employees);
+      });
+  };
 
   if (loading) {
     return <Loading />;
@@ -57,38 +84,49 @@ export default function Employees() {
   return (
     <div>
       <Button variant="contained">
-      <Link to="/create">Create employee</Link>
+        <Link to="/create">Create employee</Link>
       </Button>
+      <Button variant="contained" onClick={handleRandomize}>
+        Randomize Height
+      </Button>
+      <input value={filteredEmployees} onChange={handleFilter} placeholder="Filter..."></input>
       <TableContainer component={Paper}>
         <TableHead>
           <TableRow>
             <TableCell component={Paper}>Name</TableCell>
             <TableCell component={Paper}>Level</TableCell>
             <TableCell component={Paper}>Position</TableCell>
+            <TableCell component={Paper}>Height</TableCell>
             <TableCell component={Paper}>Actions</TableCell>
           </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map((employee) => (
-              <TableRow key={employee._id}>
-                <TableCell>{employee.name}</TableCell>
-                <TableCell>{employee.level}</TableCell>
-                <TableCell>{employee.position}</TableCell>
-                <TableCell>
-                  <Tooltip title="Update">
+        </TableHead>
+        <TableBody>
+          {filter.map((employee) => (
+            <TableRow key={employee._id}>
+              <TableCell>{employee.name}</TableCell>
+              <TableCell>{employee.level}</TableCell>
+              <TableCell>{employee.position}</TableCell>
+              <TableCell>{employee.height}</TableCell>
+              <TableCell>
+                <Tooltip title="Update">
                   <Button variant="contained">
-                  <Link to={`/update/${employee._id}`}><Upgrade /></Link>
+                    <Link to={`/update/${employee._id}`}>
+                      <Upgrade />
+                    </Link>
                   </Button>
-                  </Tooltip>
-                  <Tooltip title="Delete">
-                  <Button variant="contained" onClick={() => handleDelete(employee._id)} >
-                  <Delete />
-                </Button>
-                  </Tooltip>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
+                </Tooltip>
+                <Tooltip title="Delete">
+                  <Button
+                    variant="contained"
+                    onClick={() => handleDelete(employee._id)}
+                  >
+                    <Delete />
+                  </Button>
+                </Tooltip>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
       </TableContainer>
     </div>
   );
